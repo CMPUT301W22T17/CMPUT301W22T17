94709c5d76f1f00edb2485265cec5668786d8aac
Steven Fan <74640860+sfan5ualberta@users.noreply.github.com>
2022-02-02 17:30:19 -0700

    Update Shape.java

--- CMPUT301W22T17/app/src/main/java/com/example/cmput301w22t17/Shape.java
+++ CMPUT301W22T17/app/src/main/java/com/example/cmput301w22t17/Shape.java
@@ -3,5 +3,5 @@ package com.example.cmput301w22t17;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    protected String color = "turqoise";
 }
