cc8143af5d457c82c5a8f0e9c2bbd649fd3f58af
Muzammil Mandary <zamu2912@gmail.com>
2022-02-02 17:38:01 -0700

    Add color black

--- CMPUT301W22T17/app/src/main/java/com/example/cmput301w22t17/Shape.java
+++ CMPUT301W22T17/app/src/main/java/com/example/cmput301w22t17/Shape.java
@@ -3,6 +3,6 @@ package com.example.cmput301w22t17;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
-    protected String color = "Black";
+    protected String color = "red";
 
 }
